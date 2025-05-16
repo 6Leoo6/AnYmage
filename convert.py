a0c105c73ea99c1327f6d13823199f65e7455764
Leó Takács <leo.takacs@yahoo.com>
2025-05-16 12:31:20 +0200

    Store filename and extension in the file

--- convert.py
+++ convert.py
@@ -1,19 +1,27 @@
 import argparse
 import math
+from os.path import basename
 
 import numpy as np
 from PIL import Image
 
 
-def convert_to_image(input_path, output_path, reverse=False):
+def convert_to_image(input_path, output_path=None, reverse=False):
 
     if not reverse:
         with open(input_path, 'rb') as f:
             file_bytes = bytearray(f.read())
 
         useful_bytes = file_bytes
-        number_of_bytes = len(useful_bytes) + 3
+
+        filename = basename(input_path)
+        filename_bytes = bytearray(filename.encode('utf-8'))
+
+        number_of_bytes = len(useful_bytes) + len(filename_bytes) + 3 + 3 # File + Filename + Number of bytes containing the name + Number of filler bytes
+
         number_of_useful_pixels = math.ceil(number_of_bytes / 3)
+
+
 
 
         smallest_image_dim = math.ceil(math.sqrt(
@@ -22,18 +30,26 @@ def convert_to_image(input_path, output_path, reverse=False):
         min_number_of_pixels = smallest_image_dim ** 2
         min_number_of_bytes = min_number_of_pixels * 3
 
-        number_of_leftover_bytes = int(min_number_of_bytes - number_of_bytes)
+        number_of_filler_bytes = int(min_number_of_bytes - number_of_bytes)
 
-        file_suffix = bytearray(number_of_leftover_bytes.to_bytes(3, byteorder="big"))
-        filler_bytes = bytearray(number_of_leftover_bytes * b'\x00')
+        filler_bytes_length = bytearray(number_of_filler_bytes.to_bytes(3, byteorder="big"))
+        filler_bytes = bytearray(number_of_filler_bytes * b'\x00')
+
+        filename_bytes_length = bytearray(len(filename_bytes).to_bytes(3, byteorder="big"))
 
         output_bytes = np.array(
-            useful_bytes + filler_bytes + file_suffix,
+            useful_bytes + filename_bytes + filename_bytes_length + filler_bytes + filler_bytes_length,
         )
 
         rgb_array = output_bytes.reshape((smallest_image_dim, smallest_image_dim, 3))
 
-        Image.fromarray(rgb_array).save(output_path)
+        img = Image.fromarray(rgb_array)
+        if output_path:
+            img.save(output_path)
+        else:
+            image_filename = filename.rsplit(".", 1)[0] + ".png"
+
+            img.save(image_filename)
     else:
         image_bytes = bytearray(Image.open(input_path).tobytes())
         filler_length = int.from_bytes(image_bytes[-3:])
@@ -50,9 +66,12 @@ if __name__ == '__main__':
     )
 
     parser.add_argument("-i", "--input", nargs=1, required=True, help="Input file")
-    parser.add_argument("-o", "--output", nargs=1, required=True, help="Output file")
+    parser.add_argument("-o", "--output", nargs=1, help="Output file")
     parser.add_argument("-r", "--reverse", action="store_true", help="Reverse the process")
 
     args = parser.parse_args()
 
-    convert_to_image(args.input[0], args.output[0], args.reverse)
+    if args.output:
+        convert_to_image(args.input[0], output_path=args.output[0], reverse=args.reverse)
+    else:
+        convert_to_image(args.input[0], reverse=args.reverse)
